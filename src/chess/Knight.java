00bbe105e3f4daf6d232cf1e72294bdd4f9535ca
Karim Smires <96136962+ks1686@users.noreply.github.com>
2024-02-06 19:56:53 -0500

    New pieces/classes
    Pawn and Knight have joined the party!

---
+++ src/chess/Knight.java
@@ -0,0 +1,24 @@
+package chess;
+
+public class Knight extends Piece {
+
+    public Knight(boolean isWhite) {
+        this.isWhite = isWhite;
+        if (isWhite) {
+            this.pieceType = PieceType.WN;
+        } else {
+            this.pieceType = PieceType.BN;
+        }
+    }
+
+    public boolean canMove(int rank, int file, int newRank, int newFile, boolean isNewSpotEmpty) {
+        int rankChange = Math.abs(rank - newRank);
+        int fileChange = Math.abs(file - newFile);
+        if ((rankChange == 2 && fileChange == 1) || (rankChange == 1 && fileChange == 2)) {
+            // perfect L
+            return true;
+        }
+        // invalid move not accounting for pieces in the way
+        return false;
+    }
+}
---
+++ src/chess/Pawn.java
@@ -0,0 +1,43 @@
+package chess;
+
+public class Pawn extends Piece {
+
+    public Pawn(boolean isWhite) {
+        this.isWhite = isWhite;
+        if (isWhite) {
+            this.pieceType = PieceType.WP;
+        } else {
+            this.pieceType = PieceType.BP;
+        }
+    }
+
+    public boolean canMove(int rank, int file, int newRank, int newFile, boolean isNewSpotEmpty) {
+        int rankChange;
+        int fileChange;
+        boolean isAllowed = false; // default to false for legal move
+
+        // check if the pawn is moving up or down
+        if (this.isWhite) {
+            // white pawn, moves up
+            rankChange = newRank - rank;
+        } else {
+            // black pawn, moves down
+            rankChange = -(newRank - rank);
+        }
+
+        fileChange = Math.abs(file - newFile);
+        // check if the move is valid for a pawn
+        if (rankChange == 1 && fileChange == 0 && isNewSpotEmpty) {
+            // pawn can move forward one spot
+            isAllowed = true;
+        } else if (this.hasMoved == false && rankChange == 2 && fileChange == 0 && isNewSpotEmpty) {
+            // pawn can move forward two spots
+            isAllowed = true;
+        } else if (rankChange == 1 && fileChange == 0 && isNewSpotEmpty == false) {
+            // pawn can capture, en passant
+            isAllowed = true;
+        }
+        return isAllowed;
+    }
+
+}
