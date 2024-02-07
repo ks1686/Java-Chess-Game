c6b74cf1eba8ffd12f4e10bf988742ca3377f643
Karim Smires <96136962+ks1686@users.noreply.github.com>
2024-02-06 19:09:10 -0500

    Barebones Queen class.

--- src/chess/Queen.java
+++ src/chess/Queen.java
@@ -14,4 +14,26 @@ public class Queen extends Piece {
         }
     }
 
+    // check if the move is valid for a queen
+    public boolean canMove(int rank, int file, int newRank, int newFile, boolean isNewSpotEmpty) {
+        int diagX = Math.abs(rank - newRank);
+        int diagY = Math.abs(file - newFile);
+        if (rank == newRank || file == newFile || diagX == diagY) {
+            // perfect diagonal
+            return true;
+        } else if (diagX == 0 && diagY != 0) {
+            // perfect vertical
+            return true;
+        } else if (diagY == 0 && diagX != 0) {
+            // perfect horizontal
+            return true;
+        }
+        // invalid move not accounting for pieces in the way
+        return false;
+    }
+
+    public void movePiece() {
+        this.hasMoved = true;
+    }
+
 }
