978877f00f86d8ba0b53c68439396113b02e9920
jacobjude <jacob.jude03@gmail.com>
2024-02-08 22:14:23 -0500

    Simplified Chess.start(), checked for pawn promotion in .play()

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.ArrayList;
 
+
 //!Cannot modify this class
 class ReturnPiece {
 	static enum PieceType {
@@ -54,7 +55,7 @@ public class Chess {
 
 	enum Player {
 		white, black
-	}
+	};
 
 	private static Player currentPlayer = chess.Chess.Player.white; // current player
 
@@ -72,10 +73,17 @@ public class Chess {
 		}
 
 		// move input formatting
+		move = move.trim(); // remove trailing and leading whitespace
 		String fromFile = move.substring(0, 1);
 		int fromRank = Integer.parseInt(move.substring(1, 2));
 		String toFile = move.substring(3, 4);
 		int toRank = Integer.parseInt(move.substring(4, 5));
+
+		if (move.length() >= 7) { // move looks like "e4 e5 N"
+			String pawnPromotion = move.substring(7,8);
+		} else {
+			String pawnPromotion = null;
+		}
 
 		// Check if player even made a move
 		if (fromFile.equals(toFile) && fromRank == toRank) {
@@ -92,6 +100,49 @@ public class Chess {
 
 		play = null;
 		return play;
+	}
+
+	private static ReturnPiece createNewPiece(String pieceType, boolean isWhite){
+		switch (pieceType) {
+			case "P": return new Pawn(isWhite);
+			case "R": return new Rook(isWhite);
+			case "N": return new Knight(isWhite);
+			case "B": return new Bishop(isWhite);
+			case "Q": return new Queen(isWhite);
+			case "K": return new King(isWhite);
+			default: throw new IllegalArgumentException("Invalid piece type: " + pieceType);
+
+		}
+	}
+
+	private static void setupTeam(ReturnPlay play, String[] pieceOrder, boolean isWhite) {
+		ReturnPiece.PieceFile[] fileOrder = ReturnPiece.PieceFile.values();
+		int backrank;
+		int pawnRank;
+
+		if (isWhite) { 
+			backrank = 1;
+			pawnRank = 2;
+		} else { 
+			backrank = 8; 
+			pawnRank = 7;
+		}
+		
+		// setup pawns for this team
+		for (ReturnPiece.PieceFile file : ReturnPiece.PieceFile.values()) {
+			ReturnPiece newPiece = createNewPiece("P", isWhite);
+			newPiece.pieceFile = file;
+			newPiece.pieceRank = pawnRank;
+			play.piecesOnBoard.add(newPiece);
+		}
+
+		// setup the back rank for this team
+		for (int i = 0; i < pieceOrder.length; i++){
+			ReturnPiece newPiece = createNewPiece(pieceOrder[i], true);
+			newPiece.pieceFile = fileOrder[0];
+			newPiece.pieceRank = backrank;
+			play.piecesOnBoard.add(newPiece);
+		}
 	}
 
 	/**
@@ -116,178 +167,9 @@ public class Chess {
 		moves.clear();
 		play.message = null;
 
-		/*
-		 * Add all pieces to the board
-		 * Some pieces have repeats, numbers go from left to right (follow the file
-		 * logic)
-		 * I'm going to be honest, there's gotta be a better way to do this, but I can't
-		 * figure it out.
-		 * Loops maybe?
-		 */
-
-		// ? -- White - ?//
-		// A1
-		ReturnPiece WR1 = new Rook(true); // boolean in constructor already defines piece enum
-		WR1.pieceFile = ReturnPiece.PieceFile.a;
-		WR1.pieceRank = 1;
-		play.piecesOnBoard.add(WR1);
-		// B1
-		ReturnPiece WN1 = new Knight(true);
-		WN1.pieceFile = ReturnPiece.PieceFile.b;
-		WN1.pieceRank = 1;
-		play.piecesOnBoard.add(WN1);
-		// C1
-		ReturnPiece WB1 = new Bishop(true);
-		WB1.pieceFile = ReturnPiece.PieceFile.c;
-		WB1.pieceRank = 1;
-		play.piecesOnBoard.add(WB1);
-		// D1
-		ReturnPiece WQ = new Queen(true);
-		WQ.pieceFile = ReturnPiece.PieceFile.d;
-		WQ.pieceRank = 1;
-		play.piecesOnBoard.add(WQ);
-		// E1
-		ReturnPiece WK = new King(true);
-		WK.pieceFile = ReturnPiece.PieceFile.e;
-		WK.pieceRank = 1;
-		play.piecesOnBoard.add(WK);
-		// F1
-		ReturnPiece WB2 = new Bishop(true);
-		WB2.pieceFile = ReturnPiece.PieceFile.f;
-		WB2.pieceRank = 1;
-		play.piecesOnBoard.add(WB2);
-		// G1
-		ReturnPiece WN2 = new Knight(true);
-		WN2.pieceFile = ReturnPiece.PieceFile.g;
-		WN2.pieceRank = 1;
-		play.piecesOnBoard.add(WN2);
-		// H1
-		ReturnPiece WR2 = new Rook(true);
-		WR2.pieceFile = ReturnPiece.PieceFile.h;
-		WR2.pieceRank = 1;
-		play.piecesOnBoard.add(WR2);
-		// A2
-		ReturnPiece WP1 = new Pawn(true);
-		WP1.pieceFile = ReturnPiece.PieceFile.a;
-		WP1.pieceRank = 2;
-		play.piecesOnBoard.add(WP1);
-		// B2
-		ReturnPiece WP2 = new Pawn(true);
-		WP2.pieceFile = ReturnPiece.PieceFile.b;
-		WP2.pieceRank = 2;
-		play.piecesOnBoard.add(WP2);
-		// C2
-		ReturnPiece WP3 = new Pawn(true);
-		WP3.pieceFile = ReturnPiece.PieceFile.c;
-		WP3.pieceRank = 2;
-		play.piecesOnBoard.add(WP3);
-		// D2
-		ReturnPiece WP4 = new Pawn(true);
-		WP4.pieceFile = ReturnPiece.PieceFile.d;
-		WP4.pieceRank = 2;
-		play.piecesOnBoard.add(WP4);
-		// E2
-		ReturnPiece WP5 = new Pawn(true);
-		WP5.pieceFile = ReturnPiece.PieceFile.e;
-		WP5.pieceRank = 2;
-		play.piecesOnBoard.add(WP5);
-		// F2
-		ReturnPiece WP6 = new Pawn(true);
-		WP6.pieceFile = ReturnPiece.PieceFile.f;
-		WP6.pieceRank = 2;
-		play.piecesOnBoard.add(WP6);
-		// G2
-		ReturnPiece WP7 = new Pawn(true);
-		WP7.pieceFile = ReturnPiece.PieceFile.g;
-		WP7.pieceRank = 2;
-		play.piecesOnBoard.add(WP7);
-		// H2
-		ReturnPiece WP8 = new Pawn(true);
-		WP8.pieceFile = ReturnPiece.PieceFile.h;
-		WP8.pieceRank = 2;
-		play.piecesOnBoard.add(WP8);
-
-		// ? -- Black - ?//
-		// A8
-		ReturnPiece BR1 = new Rook(false);
-		BR1.pieceFile = ReturnPiece.PieceFile.a;
-		BR1.pieceRank = 8;
-		play.piecesOnBoard.add(BR1);
-		// B8
-		ReturnPiece BN1 = new Knight(false);
-		BN1.pieceFile = ReturnPiece.PieceFile.b;
-		BN1.pieceRank = 8;
-		play.piecesOnBoard.add(BN1);
-		// C8
-		ReturnPiece BB1 = new Bishop(false);
-		BB1.pieceFile = ReturnPiece.PieceFile.c;
-		BB1.pieceRank = 8;
-		play.piecesOnBoard.add(BB1);
-		// D8
-		ReturnPiece BQ = new Queen(false);
-		BQ.pieceFile = ReturnPiece.PieceFile.d;
-		BQ.pieceRank = 8;
-		play.piecesOnBoard.add(BQ);
-		// E8
-		ReturnPiece BK = new King(false);
-		BK.pieceFile = ReturnPiece.PieceFile.e;
-		BK.pieceRank = 8;
-		play.piecesOnBoard.add(BK);
-		// F8
-		ReturnPiece BB2 = new Bishop(false);
-		BB2.pieceFile = ReturnPiece.PieceFile.f;
-		BB2.pieceRank = 8;
-		play.piecesOnBoard.add(BB2);
-		// G8
-		ReturnPiece BN2 = new Knight(false);
-		BN2.pieceFile = ReturnPiece.PieceFile.g;
-		BN2.pieceRank = 8;
-		play.piecesOnBoard.add(BN2);
-		// H8
-		ReturnPiece BR2 = new Rook(false);
-		BR2.pieceFile = ReturnPiece.PieceFile.h;
-		BR2.pieceRank = 8;
-		play.piecesOnBoard.add(BR2);
-		// A7
-		ReturnPiece BP1 = new Pawn(false);
-		BP1.pieceFile = ReturnPiece.PieceFile.a;
-		BP1.pieceRank = 7;
-		play.piecesOnBoard.add(BP1);
-		// B7
-		ReturnPiece BP2 = new Pawn(false);
-		BP2.pieceFile = ReturnPiece.PieceFile.b;
-		BP2.pieceRank = 7;
-		play.piecesOnBoard.add(BP2);
-		// C7
-		ReturnPiece BP3 = new Pawn(false);
-		BP3.pieceFile = ReturnPiece.PieceFile.c;
-		BP3.pieceRank = 7;
-		play.piecesOnBoard.add(BP3);
-		// D7
-		ReturnPiece BP4 = new Pawn(false);
-		BP4.pieceFile = ReturnPiece.PieceFile.d;
-		BP4.pieceRank = 7;
-		play.piecesOnBoard.add(BP4);
-		// E7
-		ReturnPiece BP5 = new Pawn(false);
-		BP5.pieceFile = ReturnPiece.PieceFile.e;
-		BP5.pieceRank = 7;
-		play.piecesOnBoard.add(BP5);
-		// F7
-		ReturnPiece BP6 = new Pawn(false);
-		BP6.pieceFile = ReturnPiece.PieceFile.f;
-		BP6.pieceRank = 7;
-		play.piecesOnBoard.add(BP6);
-		// G7
-		ReturnPiece BP7 = new Pawn(false);
-		BP7.pieceFile = ReturnPiece.PieceFile.g;
-		BP7.pieceRank = 7;
-		play.piecesOnBoard.add(BP7);
-		// H7
-		ReturnPiece BP8 = new Pawn(false);
-		BP8.pieceFile = ReturnPiece.PieceFile.h;
-		BP8.pieceRank = 7;
-		play.piecesOnBoard.add(BP8);
+		String[] pieceOrder = {"R", "N", "B", "Q", "K", "B", "N", "R"};
+		setupTeam(play, pieceOrder, true); // setup white's pawns + back rank
+		setupTeam(play, pieceOrder, false); // setup black's pawns + back rank
 
 	}
 }
