3b4f71d817b201c03168d98569285a6a6c72b642
Karim Smires <96136962+ks1686@users.noreply.github.com>
2024-02-23 10:03:07 -0500

    Queen getVisibleSquaresFromLocation()
    Implementation for Queen's getVisibleSquaresFromLocation() method completed (combined logic from Rooks and Bishops)

--- src/chess/Queen.java
+++ src/chess/Queen.java
@@ -15,10 +15,7 @@ public class Queen extends Piece {
 
   // TODO: need to implement checking for pieces in the way
   public boolean canMoveSpecific(
-      int rank,
-      ReturnPiece.PieceFile file,
-      int newRank,
-      ReturnPiece.PieceFile newFile) {
+      int rank, ReturnPiece.PieceFile file, int newRank, ReturnPiece.PieceFile newFile) {
     int rankChange = Math.abs(rank - newRank); // change in rank
     int fileChange = Math.abs(enumFileToChar(file) - enumFileToChar(newFile)); // change in file
     if (rankChange == 0 && fileChange != 0) {
@@ -32,12 +29,76 @@ public class Queen extends Piece {
     // invalid move not accounting for pieces in the way
   }
 
-  public ArrayList<ArrayList<Square>> getVisibleSquaresFromLocation(int rank, ReturnPiece.PieceFile file) {
-    // combination of rook, bishop, and king. 
-    // repeating squares should be fine 
-    // (for example, the square left of the queen would be shared w/ the king array as well as the leftRow array from the rook implementation)
-    // these squares repeat but it shouldn't cause problems if they do
+  public ArrayList<ArrayList<Square>> getVisibleSquaresFromLocation(
+      int rank, ReturnPiece.PieceFile file) {
+    ArrayList<ArrayList<Square>> visibleSquares = new ArrayList<>();
+
+    // combine logic we used for rook and bishop
+    ArrayList<Square> upperColumn = new ArrayList<>();
+    ArrayList<Square> lowerColumn = new ArrayList<>();
+    ArrayList<Square> rightRow = new ArrayList<>();
+    ArrayList<Square> leftRow = new ArrayList<>();
+    ArrayList<Square> upperRightDiagonal = new ArrayList<>();
+    ArrayList<Square> upperLeftDiagonal = new ArrayList<>();
+    ArrayList<Square> lowerRightDiagonal = new ArrayList<>();
+    ArrayList<Square> lowerLeftDiagonal = new ArrayList<>();
+
+    // upper column
+    for (int r = rank + 1; r <= Chess.MAX_RANK; r++) {
+      upperColumn.add(new Square(r, file));
+    }
+
+    // lower column
+    for (int r = rank - 1; r >= Chess.MIN_RANK; r--) {
+      lowerColumn.add(new Square(r, file));
+    }
+
+    int fileInt = file.ordinal(); // get the file as an int
+
+    // right row
+    for (int f = fileInt + 1; f < ReturnPiece.PieceFile.values().length; f++) {
+      rightRow.add(new Square(rank, ReturnPiece.PieceFile.values()[f]));
+    }
+
+    // left row
+    for (int f = fileInt - 1; f >= 0; f--) {
+      leftRow.add(new Square(rank, ReturnPiece.PieceFile.values()[f]));
+    }
+
+    // Upper Right Diagonal
+    for (int r = rank + 1, f = fileInt + 1;
+        r <= Chess.MAX_RANK && f < ReturnPiece.PieceFile.values().length;
+        r++, f++) {
+      upperRightDiagonal.add(new Square(r, ReturnPiece.PieceFile.values()[f]));
+    }
+
+    // Upper Left Diagonal
+    for (int r = rank + 1, f = fileInt - 1; r <= Chess.MAX_RANK && f >= 0; r++, f--) {
+      upperLeftDiagonal.add(new Square(r, ReturnPiece.PieceFile.values()[f]));
+    }
+
+    // Lower Right Diagonal
+    for (int r = rank - 1, f = fileInt + 1;
+        r >= Chess.MIN_RANK && f < ReturnPiece.PieceFile.values().length;
+        r--, f++) {
+      lowerRightDiagonal.add(new Square(r, ReturnPiece.PieceFile.values()[f]));
+    }
+
+    // Lower Left Diagonal
+    for (int r = rank - 1, f = fileInt - 1; r >= Chess.MIN_RANK && f >= 0; r--, f--) {
+      lowerLeftDiagonal.add(new Square(r, ReturnPiece.PieceFile.values()[f]));
+    }
+
+    // add all the arrays to the visibleSquares arraylist
+    visibleSquares.add(upperColumn);
+    visibleSquares.add(lowerColumn);
+    visibleSquares.add(rightRow);
+    visibleSquares.add(leftRow);
+    visibleSquares.add(upperRightDiagonal);
+    visibleSquares.add(upperLeftDiagonal);
+    visibleSquares.add(lowerRightDiagonal);
+    visibleSquares.add(lowerLeftDiagonal);
+
     return null;
   }
-
 }
